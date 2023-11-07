cd6ee5dc6775dd58929d0e08ab9c30dc641905e1
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-07 15:23:19 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,6 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const li = document.createElement("li");
+li.textContent = ingredients.forEach((ingredient) => ingredient)
+console.log(li.textContent);
