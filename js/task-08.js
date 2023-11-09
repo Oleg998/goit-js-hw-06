c4d6f8617fd0a88ee9d826fd7403c8c5e61be3bc
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2023-11-09 18:01:59 +0200

    done9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", onSubmit);
+function onSubmit(evt) {
+  evt.preventDefault();
+  const { email, password } = evt.currentTarget.elements;
+  const emailValue = email.value;
+  const passwordValue = password.value;
+  if (emailValue === "" || passwordValue === "") {
+    const message = " Всі поля повинні бути заповнені!!!";
+    alert(message);
+  } else {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(data);
+  }
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,16 @@
+const btnClick = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+const body = document.querySelector("body")
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btnClick.addEventListener("click", chengeColor);
+
+function chengeColor(evt) {
+   let randomColor = getRandomHexColor();
+   colorSpan.textContent = randomColor;
+   body.style.backgroundColor = randomColor;
+};
